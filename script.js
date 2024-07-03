ad66bb1db9b9efd693b4f1814058926fcdd56494
Muhammad Tayyab <131167152+786Tayyab@users.noreply.github.com>
2024-07-03 15:53:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const slideWidth = slides[0].clientWidth;
+    document.querySelector('.slides').style.transform = `translateX(${-slideWidth * currentSlide}px)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+window.addEventListener('resize', () => showSlide(currentSlide));
+showSlide(currentSlide);
